d3c12c39dbc4026826934801f3f2ab659f47a0b4
Kuba <thekarmelkowy@wp.pl>
2015-03-30 04:56:38 +0200

    wersja 4.0

--- README.md
+++ README.md
@@ -2,38 +2,58 @@ gra w życie
 ===========
 
 wymagania:
-
+---------
 * python >= 2.7
 * biblioteka wxpython (sudo apt-get install python-wxgtk2.8)
 
 funkcje:
-
+---------
 * losowe poruszanie się potworków po mapie
 * wykrywanie kolizji z innymi potworkami i omijanie się albo rozmnażanie
 * potworki mają określoną długość życia (zmienna globalna live)
 * potworki umierają
 * potworki zmieniają kolor w zależności od fazy życia (różowy - dziecko; czerwony, zielony - dorosły; szzart - starzec)
-* możliwość zatrzymywania animacji
-* prawy klawisz to gumka 
+* możliwość zatrzymywania animacji (spacja albo przycisk na dole)
+* możliwość zminay ilość cykli na sekundę ( wyrażona w odstępach pomiędzy cyklami w ms)
+* prawy klawisz to gumka ( w przyszłości dowolne narzędnie)
 * funkcje lewego klawisza wybiera się przez przyciski na dole
-* w terminalu wyświetlają się podstawowe statystyki 
+* z prawej strony wyświetlają się podstawowe statystyki 
 * można zmienić ilość i zarazem wielkość pojedyńczych pól (zmienna globalna size)
 * podział na gridy w których zmienna jest szansa na rozmnożenie
 * losowe śmieci w przypodku zbyt dużego zagęszczenia
 * można stawiać ściany blokujące ruch
+* pędzel ma zmienny rozmiar (scroll albo slider z prawej)
+* pędzel ma zmienne krycie  ( slider po prawej ) od 100% dla wartości 0 do 1% dla wartości 100
+* wyświetlenie pędzla na panelu
 
 TODO:
-
+--------
 * uzupełnić komenty
-
-
-* anomalie
-* klęski
-* jakieś guziczki do tego
  
-* wyświetlanie statystyk w frame
-* zmienić layout przycisków
-* poprawić nazewnictwo w głównym layoucie
+* poprawić problem z pędzlem w wartościach niepażystych
 
+* problem z kryciem gumki przy wartości 100%
+
+* zmioenić wartość slidera od czasu z ms na ilość klatek na s
+
+* zmioenić wartość slidera od krycia z czegośtam na %
+
+* rozwiązać problem z evt_leave_window - odpowiedzialny za znikanie pędzla
+
+* wyprowadzić jeszcze kilka zmiennych na wieszch
+
+* możliwość edycji niektórych zmiennych podczas trwania animacji
 
 * eksport do exe'ka
+
+
+Dodatkowo:
+-----------
+
+* anomalie?
+
+* klęski?
+
+* zapysywanie i przewijanie symulacji
+
+* "zadania" z ograniczeniami - masz ileś tego ileś tego, zrób kolonie
--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 # -*- coding: utf-8 -*-
 import wx, random, time
 
-#zakres losowości długości życia
-live = range(80, 110)
 # wielkość pojedyńczego obiektu im większe tym mniej ich na ekranie
-size = 5
+size = 20
+
+#średnia dłuŋość życia
+live = 90
+
+# maksymalna ilość postaci na jednym gridzie (10x10 pól)
+max_on_grid = 20
+
+# maksymalny procent dzieci w danym gridzie, gdy zostanie osiądnięty, szansa na dziecko jest zerowa
+max_per_of_child=20
 
 class Wall:
     s = size
@@ -19,7 +26,6 @@ class Wall:
         dc.SetPen(wx.Pen(self.color, 1))
         dc.SetBrush(wx.Brush(self.color, wx.SOLID))
         dc.DrawRectangle(x, y, self.s, self.s)
-
 
 
 class Person:
@@ -39,7 +45,7 @@ class Person:
         self.pos = pos
         self.sex = sex
         self.can_multiply = False
-        self.dead_age = random.choice(live)
+        self.dead_age = random.randint(live-10, live+10)
 
         self.age = 0
         self.dir = None
@@ -145,35 +151,71 @@ class MyFrame(wx.Frame):
         
         self.s = size
         # określenie wymierów mapy
-        self.w, self.h = 1200/self.s, 600/self.s
+        self.w, self.h = 1000/self.s, 600/self.s
 
+        # zmienne początkowe
         self.tools = ['man', 'woman', 'rubber', 'wall']
         self.left_tool = self.tools[0]
         self.right_tool = self.tools[2]
-        self.tool_size = 50
+        self.tool_size = 10
+        self.tool_mesh = 0
+        self.mouse_pos = (None, None)
 
         self.absolute_live_time = 0
         self.born_ratio = 0
-
 
         # dodowanie layoutu
         main_panel = wx.Panel(self, wx.ID_ANY)
         main_panel.SetFocus()
 
         box = wx.BoxSizer(wx.VERTICAL)
+        h_box = wx.BoxSizer(wx.HORIZONTAL)
+        right_box = wx.BoxSizer(wx.VERTICAL)
+        box.Add(h_box)
         self.panel = wx.Panel(main_panel, wx.ID_ANY, size=(self.w*self.s, self.h*self.s))
-        box.Add(self.panel, 1, wx.EXPAND)
+        #self.panel.Bind(wx.EVT_LEAVE_WINDOW, self.OnMouseOut)
+        h_box.Add(self.panel, 1, wx.EXPAND)
+        h_box.Add(right_box, 1, wx.EXPAND)
+
+        self.statistics = wx.StaticText(main_panel, -1)
+        self.statistics.SetLabel("statystyki")
+        right_box.Add(self.statistics, 1, wx.EXPAND)
+
+
+        label1 = wx.StaticText(main_panel, -1, "Wielkość pędzla:")
+        right_box.Add(label1, 0)
+
+        self.tool_size_slider = wx.Slider(main_panel, -1, 10, 1, 100,
+            style=wx.SL_HORIZONTAL |wx.SL_LABELS, size=(280, -1))
+        right_box.Add(self.tool_size_slider, 0)
+        self.tool_size_slider.Bind(wx.EVT_SLIDER, self.changeToolSize, self.tool_size_slider)
+
+        label2 = wx.StaticText(main_panel, -1, "Rozmycie pędzla:")
+        right_box.Add(label2, 0)
+
+        self.tool_mesh_slider = wx.Slider(main_panel, -1, 0, 0, 100,
+            style=wx.SL_HORIZONTAL | wx.SL_LABELS, size=(280, -1))
+        right_box.Add(self.tool_mesh_slider, 0)
+        self.tool_mesh_slider.Bind(wx.EVT_SLIDER, self.changeToolMesh, self.tool_mesh_slider)
+
 
         button_box = wx.BoxSizer(wx.HORIZONTAL)
 
-        stop_play_button = wx.Button(main_panel, -1, 'Stop')
-        button_box.Add(stop_play_button, 1, wx.EXPAND)
-        self.Bind(wx.EVT_BUTTON, self.playStop, stop_play_button)
+        self.stop_play_button = wx.Button(main_panel, -1, 'Stop')
+        button_box.Add(self.stop_play_button, 1, wx.EXPAND)
+        self.Bind(wx.EVT_BUTTON, self.playStop, self.stop_play_button)
+
+
+        self.time_slider = wx.Slider(main_panel, -1, 100, 5, 1000,
+            style=wx.SL_HORIZONTAL | wx.SL_AUTOTICKS | wx.SL_LABELS )
+        button_box.Add(self.time_slider, 1, wx.EXPAND)
+        self.time_slider.Bind(wx.EVT_SLIDER, self.changeTimer, self.time_slider)
 
         for tool in self.tools:
             tool_btn = wx.Button(main_panel, -1, tool)
             button_box.Add(tool_btn, 1, wx.EXPAND)
             self.Bind(wx.EVT_BUTTON, self.toolChange, tool_btn)
+
 
         box.Add(button_box, 0)
 
@@ -182,40 +224,46 @@ class MyFrame(wx.Frame):
 
         # bindowanie eventów
         self.panel.Bind(wx.EVT_MOUSE_EVENTS, self.OnMouse)
+        self.panel.Bind(wx.EVT_MOTION, self.OnMouseMove)
+        self.panel.Bind(wx.EVT_MOUSEWHEEL, self.OnMouseWheel)
 
         main_panel.Bind(wx.EVT_KEY_DOWN, self.OnKeyPress)
-        self.panel.Bind( wx.EVT_PAINT, self.OnPaint )
+        self.panel.Bind( wx.EVT_PAINT, self.OnPaint)
 
         self.timer = wx.Timer(self)
-        self.timer.Start(5)
+        self.timer.Start(100)
         self.Bind(wx.EVT_TIMER, self.OnTimer, self.timer)
 
-        # aby zmienić prędkość
-        #self.timer.Stop()
-        #self.timer.Start(100)
 
-        self.SetSize((self.w*self.s, self.h*self.s+50))
+        self.SetSize((self.w*self.s+300, self.h*self.s+50))
         self.Refresh()
         self.Centre()
 
 
         self.map = Map2d()
 
-        for n in range(20):
-            pos = (n,n)
-            self.spawnPerson(pos, "woman")
 
-        for n in range(20):
-            pos = (self.w-n,self.h-n)
-            self.spawnPerson(pos, "man")
+    def changeToolMesh(self, evt):
+        mesh = self.tool_size_slider.GetValue()
+        self.tool_mesh = mesh
+        evt.Skip()
 
+    def changeToolSize(self, evt):
+        size = self.tool_size_slider.GetValue()
+        self.tool_size = size
+        evt.Skip()
 
+    def changeTimer(self, evt):
+        time = self.time_slider.GetValue()
+        self.timer.Stop()
+        self.timer.Start(int(time))
+        evt.Skip()
 
     def toolChange(self, evt):
         self.left_tool = evt.GetEventObject().GetLabel()
 
     def playStop(self, evt):
-        button = evt.GetEventObject()
+        button = self.stop_play_button
         if self.timer.IsRunning():
             self.timer.Stop()
             button.SetLabel("Start")
@@ -223,54 +271,15 @@ class MyFrame(wx.Frame):
             button.SetLabel("Stop")
             self.timer.Start()
 
-    def rubber(self, pos):
-        for x in range(self.tool_size):
-            for y in range(self.tool_size):
-                new_pos = pos[0]+x-self.tool_size/2, pos[1]+y-self.tool_size/2
-                obj_list = self.map.mapa.get(new_pos, None)
-                if obj_list is not None:
-                    for obj in obj_list:
-                        if isinstance(obj, Person):
-                            self.map.people.remove(obj)
-                        else:
-                            self.map.objects.remove(obj)
-                        del obj
 
 
-    def reCountBornMap(self):
-        #{grid:[n_dzieci, n_dorosłych]}
-        # {(0,0):[3,2]}
-        map = {}
-        for obj in self.map.people:
-            x, y = obj.pos
-            grid = x/10, y/10
-            n_child, n_parnt = map.get(grid, (0, 0))
-            if obj.age < 10:
-                n_child += 1
-            elif obj.can_multiply:
-                n_parnt +=1
-            map[grid] = n_child, n_parnt
 
-        self.born_ratio_map = {}
-        self.grid_to_clear = []
-        for grid, values in map.iteritems():
-            n_child, n_parnt = values
-            if n_parnt+n_child > 20:
-                self.grid_to_clear.append(grid)
-
-            parents = n_child+n_parnt
-            if parents > 0:
-                ratio = float(n_child)/(parents)
-            else:
-                ratio = 0
-            self.born_ratio_map[grid] = ratio
 
     def spawnWall(self, pos, color="BLACK", instant=False):
         obj = Wall()
         obj.pos = pos
         obj.color = color
         self.map.Add(obj, instant)
-
 
     def spawnPerson(self, pos, sex, instant=False, age=0):
         obj = Person()
@@ -319,7 +328,7 @@ class MyFrame(wx.Frame):
                     grid = (new_pos[0]/10, new_pos[1]/10)
                     ratio = self.born_ratio_map[grid]
                     ratio = int((1 - ratio) * 100)
-                    born_chance = random.randint(80, 100)
+                    born_chance = random.randint(100-max_per_of_child, 100)
 
                     if born_chance < ratio:
                         child_sex = random.choice(('man', 'woman'))
@@ -335,30 +344,88 @@ class MyFrame(wx.Frame):
         # przeliczenie mapy kolizji
         #self.map.CreateColisionMap()
 
+    def reCountBornMap(self):
+        #{grid:[n_dzieci, n_dorosłych]}
+        # {(0,0):[3,2]}
+        map = {}
+        for obj in self.map.people:
+            x, y = obj.pos
+            grid = x/10, y/10
+            n_child, n_parnt = map.get(grid, (0, 0))
+            if obj.age < obj.child_age:
+                n_child += 1
+            elif obj.can_multiply:
+                n_parnt +=1
+            map[grid] = n_child, n_parnt
+
+        self.born_ratio_map = {}
+        self.grid_to_clear = []
+        for grid, values in map.iteritems():
+            n_child, n_parnt = values
+            if n_parnt+n_child > max_on_grid:
+                self.grid_to_clear.append(grid)
+
+            parents = n_child+n_parnt
+            if parents > 0:
+                ratio = float(n_child)/(parents)
+            else:
+                ratio = 0
+            self.born_ratio_map[grid] = ratio
+
+
+
+
+    def OnMouseWheel(self, evt):
+        if evt.GetWheelRotation() > 0 and self.tool_size > 1:
+            self.tool_size -= 1
+        elif evt.GetWheelRotation() < 0 and self.tool_size < 100:
+            self.tool_size += 1
+        self.tool_size_slider.SetValue(self.tool_size)
+        self.Refresh()
+
+    def OnMouseOut(self, evt):
+        self.mouse_pos = (None, None)
+
+    def OnMouseMove(self, evt):
+        mouse_pos = evt.GetPosition()
+        x, y = mouse_pos
+        x /= self.s
+        y /= self.s
+        self.mouse_pos = x, y
+        self.Refresh()
+
     def OnMouse(self,event):
-        self.mouse_pos = event.GetPosition()
-        x, y = self.mouse_pos
+        mouse_pos = event.GetPosition()
+        x, y = mouse_pos
         x /= self.s
         y /= self.s
         pos = x, y
         self.map.CreateColisionMap()
-        if event.LeftIsDown():
-            if self.left_tool == self.tools[0] or self.left_tool == self.tools[1]:
-                for x in range(self.tool_size):
-                    for y in range(self.tool_size):
-                        if random.randint(0, 30) == 0:
-                            new_pos = pos[0]-self.tool_size/2+x, pos[1]-self.tool_size/2+y
-                            self.spawnPerson(new_pos, self.left_tool, age=11)
-            elif self.left_tool == 'rubber':
-                self.rubber(pos)
-            elif self.left_tool == 'wall':
-                self.spawnWall(pos, "BROWN")
-
+        tool = None
         if event.RightIsDown():
-            if self.right_tool == self.tools[0] or self.right_tool == self.tools[1]:
-                self.spawnPerson(pos, self.right_tool)
-            elif self.right_tool == 'rubber':
-                self.rubber(pos)
+            tool = self.right_tool
+        elif event.LeftIsDown():
+            tool = self.left_tool
+
+        if tool is not None:
+            for x in range(self.tool_size):
+                for y in range(self.tool_size):
+                    if random.randint(0, self.tool_mesh) == 0:
+                        new_pos = pos[0]-self.tool_size/2+x, pos[1]-self.tool_size/2+y
+                        if tool in [self.tools[0], self.tools[1]]:
+                            self.spawnPerson(new_pos, tool, age=11)
+                        elif tool == 'wall':
+                            self.spawnWall(new_pos, "BROWN")
+                        elif tool == 'rubber':
+                            obj_list = self.map.mapa.get(new_pos, None)
+                            if obj_list is not None:
+                                for obj in obj_list:
+                                    if isinstance(obj, Person):
+                                        self.map.people.remove(obj)
+                                    else:
+                                        self.map.objects.remove(obj)
+                                    del obj
+
         self.Refresh()
 
     def OnTimer(self, event):
@@ -404,12 +471,13 @@ class MyFrame(wx.Frame):
         self.time_of_cykl = time.time() - start
         self.timer.Start()
 
-
-
     def OnKeyPress(self, event):
         key_code = event.GetKeyCode()
         if key_code == wx.WXK_ESCAPE:
-            exit()            
+            self.timer.Stop()
+            exit()
+        elif key_code == wx.WXK_SPACE:
+            self.playStop(-1)
 
     def OnPaint(self, event):
         self.reCountBornMap()
@@ -426,10 +494,10 @@ class MyFrame(wx.Frame):
             ratio = int((1 - ratio) * 10)
             x, y = grid[0]*10*self.s, grid[1]*10*self.s
             dc.DrawText(str(ratio), x, y)
-        '''
         for grid in self.grid_to_clear:
             x, y = grid[0]*10*self.s, grid[1]*10*self.s
             dc.DrawText("clear", x, y)
+        '''
 
         n_child = 0
         n_man = 0
@@ -449,16 +517,27 @@ class MyFrame(wx.Frame):
                 n_grandpa += 1
 
         sum_populacji = n_child + n_man + n_woman + n_grandpa + 0.1
-
-        print 3*"\n"
-
-        print "cykl zegara: %d  , czas cyklu: %f" % (self.absolute_live_time, self.time_of_cykl)
-        print "dzieci: %d\nkobiet: %d\nmężczyzn: %d\ndziadków: %d" % (n_child, n_woman, n_man, n_grandpa)
-        print "w sumie: %d" % (sum_populacji)
+        out_text = ""
+        out_text += "cykl zegara: %d\n" % (self.absolute_live_time)
+        out_text += "dzieci: %d\nkobiet: %d\nmężczyzn: %d\ndziadków: %d\n" % (n_child, n_woman, n_man, n_grandpa)
+        out_text += "w sumie: %d\n" % (sum_populacji)
         if sum_populacji > 0:
-            print "srednia wieku: %d" % (sum_wieku/sum_populacji)
+            out_text += "srednia wieku: %d" % (sum_wieku/sum_populacji)
         else:
-            print "srednia wieku: nikt nie żyje"
+            out_text += "srednia wieku: nikt nie żyje"
+
+        self.statistics.SetLabel(out_text)
+
+
+        if self.mouse_pos[0] is not None:
+            x, y = self.mouse_pos
+            x *= self.s
+            y *= self.s
+            x = x - self.tool_size*self.s/2
+            y = y - self.tool_size*self.s/2
+            dc.SetPen(wx.Pen("BLACK", 1))
+            dc.SetBrush(wx.Brush("WHITE", style=wx.TRANSPARENT))
+            dc.DrawRectangle(x, y, self.tool_size*self.s, self.tool_size*self.s)
 
     def __repr__(self):
         return str(self.mapa)
